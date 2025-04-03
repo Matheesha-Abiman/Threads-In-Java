0b8b538e2b4a99ea12b56fd00baf48845d9f186b
Matheesha Abiman <matheeshaabi@gmail.com>
2025-04-03 15:10:27 +0530

    Thread Pool

--- 08-Inter-Thread-Commiunication/src/main/java/com/example/Main.java
+++ 08-Inter-Thread-Commiunication/src/main/java/com/example/Main.java
@@ -1,10 +1,54 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        A a = new A();
 
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                a.put(i);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                a.get();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class A {
+    int num;
+    boolean valueSet = false;
+
+    synchronized void put(int num) {
+        while (valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.num = num;
+        System.out.println("put: " + num);
+        valueSet = true;
+        notify();
+    }
+
+    synchronized void get() {
+        while (!valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("get: " + num);
+        valueSet = false;
+        notify();
     }
 }
