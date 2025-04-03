413c1c4dbf60e4fea01d9bb9e40bd410e1eb9d72
Matheesha Abiman <matheeshaabi@gmail.com>
2025-04-03 11:51:51 +0530

    Join IsAlive and Lambda Expression

---
+++ 06-Thread-Priority/src/main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("A : "+Thread.currentThread().getPriority());
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("B : "+Thread.currentThread().getPriority());
+            }
+        });
+        t1.start();
+        t2.start();
+        System.out.println("t1 priority: " + t1.getPriority());
+        System.out.println("t2 priority: " + t2.getPriority());
+    }
+}
